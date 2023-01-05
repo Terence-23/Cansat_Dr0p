e0f7719a77d0d631cc828c89fa3c4fcbb82ae731
Terence-23 <jpuchalski1703@gmail.com>
2023-01-05 17:10:59 +0100

    expanding nav function

--- Python/Can/main.py
+++ Python/Can/main.py
@@ -2,20 +2,181 @@
 import comms, sensor
 import time
 import board, digitalio
+import threading
+
+
+import math
+
+def rotate_vector(pitch, roll, vector):
+    pitch = math.radians(pitch)
+    roll = math.radians(roll)
+    x, y, z = vector
+
+    rotated_x = x * math.cos(pitch) + y * math.sin(pitch)
+    rotated_y = -x * math.sin(roll) * math.sin(pitch) + y * math.cos(roll) + z * math.sin(roll)
+    rotated_z = x * math.cos(roll) * math.sin(pitch) - y * math.sin(roll) + z * math.cos(roll)
+
+    return (rotated_x, rotated_y, rotated_z)
+
+
+def calculate_angles(acceleration_x, acceleration_y, acceleration_z):
+    pitch = math.atan2(acceleration_x, math.sqrt(acceleration_y * acceleration_y + acceleration_z * acceleration_z))
+    roll = math.atan2(acceleration_y, math.sqrt(acceleration_x * acceleration_x + acceleration_z * acceleration_z))
+    pitch_degrees = math.degrees(pitch)
+    roll_degrees = math.degrees(roll)
+    return (pitch_degrees, roll_degrees)
+
+
+def calibrate(magnetometer:adafruit_lis2mdl.LIS2MDL):
+    start_time = time.monotonic()
+    hardiron_calibration = [[1000, -1000], [1000, -1000], [1000, -1000]]
+
+    # Update the high and low extremes
+    while time.monotonic() - start_time < 10.0:
+        magval = magnetometer.magnetic
+        print("Calibrating - X:{0:10.2f}, Y:{1:10.2f}, Z:{2:10.2f} uT".format(*magval))
+        for i, axis in enumerate(magval):
+            hardiron_calibration[i][0] = min(hardiron_calibration[i][0], axis)
+            hardiron_calibration[i][1] = max(hardiron_calibration[i][1], axis)
+    print("Calibration complete:")
+    print("hardiron_calibration =", hardiron_calibration)
+
+    # Calculate biases and scaling factors for the x-axis
+    bias_x = (hardiron_calibration[0][0] + hardiron_calibration[0][1]) / 2
+    scaling_factor_x = (hardiron_calibration[0][1] - hardiron_calibration[0][0]) / 2
+
+    # Calculate biases and scaling factors for the y-axis
+    bias_y = (hardiron_calibration[1][0] + hardiron_calibration[1][1]) / 2
+    scaling_factor_y = (hardiron_calibration[1][1] - hardiron_calibration[1][0]) / 2
+
+    # Calculate biases and scaling factors for the z-axis
+    bias_z = (hardiron_calibration[2][0] + hardiron_calibration[2][1]) / 2
+    scaling_factor_z = (hardiron_calibration[2][1] - hardiron_calibration[2][0]) / 2
+
+    # Assign biases to the offset property
+    magnetometer.offset = (bias_x, bias_y, bias_z)
+
+    # Assign scaling factors to the scale property
+    magnetometer.scale = (scaling_factor_x, scaling_factor_y, scaling_factor_z)
+
+    return magnetometer
+
+
+def compass_reading(magnetometer_x, magnetometer_y, _=None):
+    # Convert the magnetometer readings to radians
+    magnetometer_x_rad = math.radians(magnetometer_x)
+    magnetometer_y_rad = math.radians(magnetometer_y)
+    
+    # Calculate the yaw angle in radians
+    yaw = math.atan2(magnetometer_y_rad, magnetometer_x_rad)
+    
+    # Convert the yaw angle to degrees
+    yaw_degrees = math.degrees(yaw)
+    
+    # Normalize the compass reading to a range of 0-360 degrees
+    compass_reading = (yaw_degrees + 360) % 360
+    
+    return compass_reading  
+
+
+def get_rotation(point1, point2):
+    x1, y1 = point1
+    x2, y2 = point2
+
+    # Calculate the difference between the points
+    dx = x2 - x1
+    dy = y2 - y1
+
+    # Calculate the angle from the positive x-axis
+    angle = math.atan2(dy, dx)
+
+    # Convert the angle from radians to degrees
+    angle = math.degrees(angle)
+
+    # Make sure the angle is positive
+    if angle < 0:
+        angle += 360
+
+    return angle
+
+
+def get_rotation_difference(current_heading, desired_heading):
+    difference = desired_heading - current_heading
+    if difference > 180:
+        difference -= 360
+    elif difference < -180:
+        difference += 360
+    return difference
+
+last_rotate = 0
 
 def nav(packet: comms.Packet):
-    # placeholder
+    global e, last_rotate
     print(packet)
+    # 
+    if time.time() < last_rotate + 0.5:
+        return
 
+    last_rotate = time.time()
+
+    mag_corected = rotate_vector(pitch, roll, lsm.getMagnetic())
+    compass = compass_reading(*mag_corected)
+    rotation = get_rotation((gps.getLat(), gps.getLon()), desiredPos)
+    rotation_to_do = get_rotation_difference(compass, rotation)
+    if rotation_to_do < -e:
+        # go Left
+        def repeat_function():
+            #replace function with function to rotate servo by 45 degrees
+            function()
+            start = time.time()
+            des_rotation = get_rotation((gps.getLat(), gps.getLon()), desiredPos)
+            while compass_reading(*rotate_vector(pitch, roll, lsm.getMagnetic())) < \
+                des_rotation - e and time.time() < start+ 0.5:
+                pass
+                # Add a delay if necessary
+                time.sleep(0.020)
+            function()
+        
+    elif rotation_to_do > e:
+        # go Right
+        def repeat_function():
+            #replace function with function to rotate servo by 45 degrees
+            function()
+            start = time.time()
+            des_rotation = get_rotation((gps.getLat(), gps.getLon()), desiredPos)
+            while compass_reading(*rotate_vector(pitch, roll, lsm.getMagnetic())) > \
+                des_rotation + e and time.time() < start+ 0.5:
+                pass
+                # Add a delay if necessary
+                time.sleep(0.020)
+            function()
+        
+    else:
+        def repeat_function():
+            pass
+
+    # Create a new thread and start it
+    thread = threading.Thread(target=repeat_function)
+    thread.start()
+
+
+# rotation acceptable error (degrees)
+e = 4
 
 def init():
-    global lsm, bme, gps, radio
+    global lsm, bme, gps, radio, pitch, roll, desiredPos
+    desiredPos = (0,0)
     lsm = sensor.LSM303()
     bme = sensor.BME(i2c=board.I2C())
     comms.SD_o = comms.SD('log.out')
     sensor.SD_o = comms.SD_o
     gps = sensor.L76x()
     radio = comms.Radio(comms.CS, comms.RESET, comms.PWR, comms.FREQ)
+    time.sleep(15)
+    pitch, roll = calculate_angles(*lsm.getAcceleration())
+    time.sleep(4)
+    lsm.mag = calibrate(lsm.mag)
+
 
 def update():
     gps.refresh()
@@ -27,14 +188,17 @@ def update():
     comms.SD_o.write(packet)
     _in = radio.recv()
     comms.SD_o.write(_in)
-    Packet.decode(_in)
+    packet = comms.Packet()
+    packet.decode(_in)
+    bme.setSeaLevelPressure(packet.pressure if packet.pressure != '' else bme.getSeaLevelPressure())
+    desiredPos = packet.gps_position if packet.gps_position != '' else desiredPos
     nav(Packet)
+
 
 def main():
     init()
     while 1:
         update()
-
 
 
 if __name__ =="__main__":
