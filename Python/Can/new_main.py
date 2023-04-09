c94ec75458547450686aef5e1745adf3d5d97ba9
terence-23 <jpuchalski1703@gmail.com>
2023-04-09 16:54:07 +0200

    add sleeping prints

--- Python/Can/new_main.py
+++ Python/Can/new_main.py
@@ -155,7 +155,7 @@ def wake_checker(lsm, bme, sleeping):
     print('start wake checker')
     while True:
         time .sleep(0.1)
-        print(sleeping.value)
+        print(sleeping.value ,'sleeping')
         if not sleeping.value and wake_check(lsm, bme):
             event_q.put(Packet.create_command_packet(time.time(), Command.WAKE).encode())
             
