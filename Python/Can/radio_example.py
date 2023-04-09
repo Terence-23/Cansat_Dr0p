8dbe78fb5f92affaf02e19ec1c1d9887ce141041
terence-23 <jpuchalski1703@gmail.com>
2023-04-09 15:47:25 +0200

    add packet system to radio_example

--- Python/Can/radio_example.py
+++ Python/Can/radio_example.py
@@ -7,74 +7,32 @@ import time
 import traceback
 
 import adafruit_rfm9x
+from new_Packet import Packet, PacketType, Command
 
 
-# Define radio parameters.
 RADIO_FREQ_MHZ = 433.0  # Frequency of the radio in Mhz. Must match your
-# module! Can be a value like 915.0, 433.0, etc.
 
-# Define pins connected to the chip, use these if wiring up the breakout according to the guide:
 CS = digitalio.DigitalInOut(board.D22)
 RESET = digitalio.DigitalInOut(board.D27)
-# Or uncomment and instead use these if using a Feather M0 RFM9x board and the appropriate
-# CircuitPython build:
-# CS = digitalio.DigitalInOut(board.RFM9X_CS)
-# RESET = digitalio.DigitalInOut(board.RFM9X_RST)
-
-# Define the onboard LED
-# LED = digitalio.DigitalInOut(board.D13)
-# LED.direction = digitalio.Direction.OUTPUT
-
-# Initialize SPI bus.
 spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 
-# Initialze RFM radio
 rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, RADIO_FREQ_MHZ)
 
-# Note that the radio is configured in LoRa mode so you can't control sync
-# word, encryption, frequency deviation, or other settings!
-
-# You can however adjust the transmit power (in dB).  The default is 13 dB but
-# high power radios like the RFM95 can go up to 23 dB:
 rfm9x.tx_power = 20
 
 
 while True:
   try:
-    # Send a packet.  Note you can only send a packet up to 252 bytes in length.
-    # This is a limitation of the radio packet size, so if you need to send larger
-    # amounts of data you will need to break it into smaller send calls.  Each send
-    # call will wait for the previous one to finish before continuing.
-    rfm9x.send(bytes(f";{time.ctime()};;;;;;;;", "utf-8"))
+    rfm9x.send(bytes(str(Packet.create_base_packet(time.time(), 30, 1013,40,0)), "ascii"))
     print("Sent Hello World message!")
 
-    # Wait to receive packets.  Note that this library can't receive data at a fast
-    # rate, in fact it can only receive and process one 252 byte packet at a time.
-    # This means you should only use this for low bandwidth scenarios, like sending
-    # and receiving a single message at a time.
     print("Waiting for packets...")
 
     packet = rfm9x.receive()
-    # Optionally change the receive timeout from its default of 0.5 seconds:
-    # packet = rfm9x.receive(timeout=5.0)
-    # If no packet was received during the timeout then None is returned.
     if packet is None:
-        # Packet has not been received
-        # LED.value = False
         print("Received nothing! Listening again...")
     else:
-        # Received a packet!
-        # LED.value = True
-        # Print out the raw bytes of the packet:
         print("Received (raw bytes): {0}".format(packet))
-        # And decode to ASCII text and print it too.  Note that you always
-        # receive raw bytes and need to convert to a text format like ASCII
-        # if you intend to do string processing on your data.  Make sure the
-        # sending side is sending ASCII data before you try to decode!
-        #packet_text = str(packet, "ascii")
-        #print("Received (ASCII): {0}".format(packet_text))
-        # Also read the RSSI (signal strength) of the last received message and
-        # print it.
         with open('log.out', 'a') as f:
             f.write(f"{packet}")
         rssi = rfm9x.last_rssi
