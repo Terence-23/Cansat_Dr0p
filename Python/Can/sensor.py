6ad71540abcf115c8da0cecb43a47e1e76167d58
Terence-23 <93096996+Terence-23@users.noreply.github.com>
2023-06-16 13:58:06 +0200

    Update sensor.py

--- Python/Can/sensor.py
+++ Python/Can/sensor.py
@@ -1,8 +1,6 @@
 # file for sensor communication
 
 from multiprocessing import Value, Array, Process, Queue
-import os
-import glob
 import adafruit_lsm303_accel
 import adafruit_lis2mdl
 import time
@@ -16,22 +14,22 @@ import adafruit_bme680
 from adafruit_onewire.bus import OneWireBus
 from adafruit_ds18x20 import DS18X20
 import movingavg
+import os
+import glob
+
 SD_o: comms.SD
 PRESSUREHPA = 1013
 
 
 class BME:
     sensor: adafruit_bme680.Adafruit_BME680
-    temp = -1000
-    press = -1000
-    hum = -1000
-    alt = -1000
+    temp = Value('d',-1000)
+    press = Value('d',-1000)
+    hum = Value('d',-1000)
+    alt = Value('d',-1000)
 
-    temp_path = 'Data/temp.out'
-    # pressure and altitude in the same file
-    press_path = 'Data/press.out'
-    hum_path = 'Data/hum.out'
-
+    log_path = 'Data/BME.out'
+    
     def __init__(self, cs=digitalio.DigitalInOut(board.D22), spi=board.SPI(), i2c=None) -> None:
         if i2c != None:
             self.sensor = adafruit_bme680.Adafruit_BME680_I2C(
@@ -57,21 +55,18 @@ class BME:
             f.write(str(self.sensor.sea_level_pressure) + '\n')
 
         while True:
-            if self.temp != self.sensor.temperature:
-                self.temp = self.sensor.temperature
-                with open(self.temp_path, 'a') as f:
-                    f.write(f'{time.time()};{self.temp}\n')
+            if self.temp.value != self.sensor.temperature or\
+                self.press.value != self.sensor.pressure or\
+                self.hum.value != self.sensor.humidity:
+                
+                self.temp.value = self.sensor.temperature
+                self.press.value = self.sensor.pressure
+                self.alt.value = self.sensor.altitude
+                self.hum.value = self.sensor.humidity
+                
+                with open(self.log_path, 'a') as f:
+                    f.write(f'{time.time()};{self.temp.value};{self.press.value};{self.hum.value};{self.alt.value}\n')
 
-            if self.press != self.sensor.pressure:
-                self.press = self.sensor.pressure
-                self.alt = self.sensor.altitude
-                with open(self.press_path, 'a') as f:
-                    f.write(f'{time.time()};{self.press};{self.alt}\n')
-
-            if self.hum != self.sensor.humdidty:
-                self.hum = self.sensor.humidity
-                with open(self.hum_path, 'a') as f:
-                    f.write(f'{time.time()};{self.hum}\n')
             time.sleep(0.199)
 
     def setSeaLevelPressure(self, pressure):
@@ -82,24 +77,23 @@ class BME:
         return self.sensor.sea_level_pressure
 
     def getAltitude(self):
-        return self.alt
+        return self.alt.value
 
     def getTemp(self):
-        return self.temp
+        return self.temp.value
 
     def getHum(self):
-        return self.hum
+        return self.hum.value
 
     def getPress(self):
-        return self.press
+        return self.press.value
 
 
 class LSM303:
 
-    acc: Tuple[float, float, float] = ()
-    magvals: Tuple[float, float, float] = ()
-    path_mag = 'Data/mag.out'
-    path_acc = 'Data/acceleration.out'
+    acc = Array('d',3)
+    magvals = Array('d',3)
+    log_path = 'Data/LSM.out'
 
     def __init__(self, i2c=board.I2C()) -> None:
         self.accel = adafruit_lsm303_accel.LSM303_Accel(i2c)
@@ -110,10 +104,10 @@ class LSM303:
         self.rProcess.start()
 
     def getAcceleration(self) -> Tuple[float, float, float]:
-        return self.acc
+        return self.acc[:]
 
     def getMagnetic(self) -> Tuple[float, float, float]:
-        return self.magvals
+        return self.magvals[:]
 
     def refresh(self):
         
@@ -124,24 +118,12 @@ class LSM303:
         avg_list = [x_avg, y_avg, z_avg]
         
         while True:
-            if self.accel.acceleration != self.acc:
-                self.acc = self.accel.acceleration
-                with open(self.path_acc, 'a')as f:
-                    f.write(f'{time.time()};{";".join(map(str, self.acc))}\n')
 
-<<<<<<< HEAD
-            if self.mag.magnetic != self.magvals:
-                self.magvals = self.mag.magnetic
-                with open(self.path_acc, 'a')as f:
-                    f.write(
-                        f'{time.time()};{";".join(map(str, self.magvals))}\n')
-=======
             if self.accel.acceleration != self.acc[:] or self.mag.magnetic != self.magvals[:]:
                 self.acc.get_obj()[:] = self.accel.acceleration
                 self.magvals.get_obj()[:] = [a.next(v) for a, v in zip(avg_list, self.mag.magnetic)]
                 with open(self.log_path, 'a')as f:
                     f.write(f'{time.time()};{";".join(map(str, self.acc[:]))};{";".join(map(str, self.magvals[:]))}\n')
->>>>>>> eebaf96 (add denoising to sensor.py)
 
             time.sleep(0.009)
 
@@ -160,7 +142,7 @@ class L76x:
         x.L76X_Send_Command(x.SET_NMEA_BAUDRATE_115200)
         time.sleep(2)
         x.L76X_Set_Baudrate(115200)
-        x.L76X_Send_Command(x.SET_POS_FIX_1S)
+        x.L76X_Send_Command(x.SET_POS_FIX_400MS)
         # Set output message
         x.L76X_Send_Command(x.SET_NMEA_OUTPUT)
         x.L76X_Exit_BackupMode()
@@ -207,6 +189,7 @@ class L76x:
     def getLon(self):
         # if self.l_rf_time + self.refresh_delay <= time.time() * 1000: self.refresh()
         return self.gps.Lon
+
 
 class Dallas:
 
