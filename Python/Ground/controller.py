1ae11074f6af24310ab78b1e43f33b2f3e6a5dba
Jerzy Puchalski <jpuchalski1703@gmail.com>
2023-06-26 21:55:35 +0200

    fix controller

--- Python/Ground/controller.py
+++ Python/Ground/controller.py
@@ -32,6 +32,6 @@ class MyController(Controller):
         print("Exiting manual mode")
 
 
-controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False)
+#controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False)
 # you can start listening before controller is paired, as long as you pair it within the timeout window
-controller.listen(timeout=60)
+#controller.listen(timeout=60)
